b56cac8d0a4f34c5f491829db1d5e0a9584bee27
Henrique Kanso Bragada <hkbragada@gmail.com>
2025-10-01 09:01:38 -0300

    Update upload_bases_ML_drive.py

--- upload_bases_ML_drive.py
+++ upload_bases_ML_drive.py
@@ -35,3 +35,43 @@ def authenticate():
             token.write(creds.to_json())
     
     return build('drive', 'v3', credentials=creds)
+
+db_url = os.getenv('URL_FRAGADB')
+engine1 = create_engine(db_url)
+
+
+def extrair_grupos_produto(engine):
+    try:
+        with engine.connect() as conn:
+
+            gp_produto_autozone = [
+                "Amortecedor de suspensão", "Atuador de embreagem", "Bandeja de suspensão", "Bobina de ignição",
+                "Bomba D''água", "Bomba de combustível", "Bucha da bandeja", "Cabo de ignição", "Cilindro auxiliar de embreagem",
+                "Cilindro mestre de embreagem", "Coxim do motor", "Disco de freio", "Filtro de ar", "Filtro de cabine", 
+                "Filtro de combustível", "Filtro de óleo", "Junta homocinética", "Junta homocinética deslizante", "Kit de reparo do amortecedor",
+                "Kit de correia", "Kit de embreagem", "Pastilha de Freio", "Pivô de suspensão", "Radiador", "Rolamento de roda",
+                "Terminal axial", "Terminal de direção", "Válvula termostática", "Vela de ignição"
+                ]
+                
+            query_grupos_produto = text(f"""
+            select 
+                gp."Id",
+                gp."Nome"
+            from 
+                "GrupoProduto" gp
+            WHERE gp."Nome" IN ({",".join(f"'{nome}'" for nome in gp_produto_autozone)})
+            """)
+            result = conn.execute(query_grupos_produto)
+
+            rows = result.fetchall()
+
+            # Convertendo o resultado para um dicionário
+            dict_grupos_produto = {row[1]: row[0] for row in rows}
+
+            print("Extração de grupos de produto realizada!")
+            return dict_grupos_produto
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+        return None
+
+extrair_grupos_produto(engine1)
